bfbf7d4ac3c9185a48704f7bdf72dbdd8cb9cbb5
kristijan <jankuloski.k@gmail.com>
2023-03-01 20:46:27 +0100

    Schoolwork 2

---
+++ homework-class02/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Please insert number:");
+bool isConverted = int.TryParse(Console.ReadLine(), out int userInput);
+if (isConverted)
+{
+    Console.WriteLine("Valid number: " + userInput);
+}
+else
+{
+    Console.WriteLine("Invalid input");
+}
