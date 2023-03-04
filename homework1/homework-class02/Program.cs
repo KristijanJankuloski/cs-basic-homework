32007fab1831342ea9ccc26406260905861f971c
kristijan <jankuloski.k@gmail.com>
2023-03-04 17:05:23 +0100

    schoolwork 3

--- homework1/homework-class02/Program.cs
+++ homework1/homework-class02/Program.cs
@@ -1,10 +1,23 @@
 ﻿Console.WriteLine("Please insert number:");
-bool isConverted = int.TryParse(Console.ReadLine(), out int userInput);
-if (isConverted)
-{
-    Console.WriteLine("Valid number: " + userInput);
-}
-else
-{
-    Console.WriteLine("Invalid input");
+int userInput = int.Parse(Console.ReadLine());
+
+//for(int i = 2;i <= userInput; i+=2)
+//{
+//    Console.WriteLine(i);
+//}
+
+//for(int i = 1; i <= userInput; i+=2)
+//{
+//    Console.WriteLine(i);
+//}
+
+for(int i = 1; i <= userInput; i++) { 
+    if(i%3 == 0 || i%7 == 0)
+        continue;
+    if(i > 100)
+    {
+        Console.WriteLine("The limit has been reached");
+        break;
+    }
+    Console.WriteLine(i);
 }
