bce7818352c0ea9746e08e558b8551611724c906
kristijan <jankuloski.k@gmail.com>
2023-03-18 20:48:24 +0100

    inheritence schoolwork

---
+++ Class06/Class06.Inheritance/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Class06.Inheritance;
+using Class06.Methods;
+
+CustomMath.Sum(2, 2);
---
+++ Class06/Class06.Methods/CustomMath.cs
@@ -0,0 +1,13 @@
+﻿namespace Class06.Methods {
+    public class CustomMath {
+        public static int Sum(int a,  int b)
+        {
+            Console.WriteLine("called");
+            return a + b;
+        }
+        public static int Square(int a)
+        {
+            return a;
+        }
+    }
+}
