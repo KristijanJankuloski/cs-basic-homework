5ffa5b113b50a7409c8c004991ba7c7df29d2f95
kristijan <jankuloski.k@gmail.com>
2023-03-21 03:07:23 +0100

    schoolwork 7

---
+++ Class06/Dictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿Dictionary<string, string> phoneBook = new Dictionary<string, string>();
+phoneBook.Add("Martin", "070/222-333");
+phoneBook.Add("John", "070/222-111");
+phoneBook.Add("Kiko", "070/555-333");
+phoneBook.Add("Bob", "070/222-123");
+phoneBook.Add("Greg", "070/298-333");
+
+Console.WriteLine("Enter a name to search: ");
+string input = Console.ReadLine();
+
+if (phoneBook.ContainsKey(input))
+{
+    Console.WriteLine(phoneBook[input]);
+}
+else
+{
+    Console.WriteLine("name was not found");
+}
